183355538ca3af90502b427b4da4d231ca12217f
davit2 <davit2@davit2.ece.vt.edu>
2017-10-11 12:26:50 -0400

    remove plot WIC SI related functions

--- track_wic_si_spot/track_max_point.py
+++ track_wic_si_spot/track_max_point.py
@@ -100,62 +100,6 @@ def find_max_intensity_point(data, mlt_range=[8, 16],
 
     return (maxval, maxloc)
 
-def iteratively_plot_WIC_SI_image(stm, etm, read_si=True, read_wic=False,
-                                  param="image", mark_max_intensity_point=True,
-                                  mlt_range=[8, 16], mlat_range=[70, 85]):
-
-    dtms, fnames = find_filenames(stm, etm, read_si=read_si, read_wic=read_wic)
-    for dtm in dtms:
-        fig = plot_WIC_SI_image(dtm, read_si=read_si, read_wic=read_wic, 
-                                mlt_range=mlt_range, mlat_range=mlat_range,
-                                param=param)
-        if read_si:
-            fdir ="../plots/track_spot/SI/"
-            fname = dtm.strftime("%H%M") + "_SI_" + param  
-        if read_wic:
-            fdir ="../plots/track_spot/WIC/"
-            fname = dtm.strftime("%H%M") + "_WIC_" + param  
-        fig.savefig(fdir + fname, dpi=300)
-    return
-        
-def plot_WIC_SI_image(dtm, read_si=True, read_wic=False,
-                      param="image", mark_max_intensity_point=True,
-                      mlt_range=[8, 16], mlat_range=[70, 85]):
-
-    import scipy
-    import cv2
-
-    fname, data = read_wic_si_data(dtm, read_si=read_si, read_wic=read_wic)
-    ss_txt = ":" + fname.split("/")[-1].split("_")[-1][4:6]
-
-    fig, ax = plt.subplots()
-    if param == "image":
-        vmin = 0; vmax = 1000; cmap = 'gist_gray'
-    if param == "mlat":
-        vmin = 60; cmap=None
-    if param == "mlon":
-        vmin=-180; vmax=180; cmap=None
-    if param == "mlt":
-        vmin=0; vmax=24; cmap=None
-
-    #img = scipy.misc.bytescale(data[param], cmin=0)
-    img = data[param]
-    if mark_max_intensity_point:
-        (maxval, maxloc) = find_max_intensity_point(data, mlt_range=mlt_range,
-                                                    mlat_range=mlat_range)
-        #cv2.circle(img, maxloc, 9, (255, 0, 0), 2)
-    cax = ax.imshow(img, cmap=cmap, vmin=vmin, vmax=vmax)
-    ax.plot(maxloc[0], maxloc[1], 'or', markersize=4)
-    ax.set_axis_off()
-    if read_si:
-        title = dtm.strftime("%Y/%m/%d %H:%M") + ss_txt + "   SI " + param
-    if read_wic:
-        title = dtm.strftime("%Y/%m/%d %H:%M") + ss_txt + "   WIC " + param
-    ax.set_title(title)
-
-    cbar = fig.colorbar(cax, shrink=0.7)
-    return fig
-
 # run the code
 def main():
     #read_si=True; read_wic=False
@@ -166,14 +110,9 @@ def main():
     stm = dt.datetime(2002, 3, 18, 15, 00) 
     etm = dt.datetime(2002, 3, 18, 17, 22) 
     fname, data = read_wic_si_data(dtm, read_si=read_si, read_wic=read_wic)
-    #fig = plot_WIC_SI_image(dtm, read_si=read_si, read_wic=read_wic, param="image")
-    #iteratively_plot_WIC_SI_image(stm, etm, read_si=read_si, read_wic=read_wic,
-    #                              param="image", mark_max_intensity_point=True,
-    #                              mlt_range=mlt_range, mlat_range=mlat_range)
     #find_max_intensity_point(data, mlt_range=mlt_range, mlat_range=mlat_range, plot_image=True)
     #fnames = find_filenames(stm, etm, read_si=True, read_wic=False)
 
-    #plt.show()
 
 if __name__ == "__main__":
     main()
