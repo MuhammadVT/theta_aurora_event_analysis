b79a3ac9105dd89ed759fe44e0826fda36e81784
MuhammadVT <mamatripik@gmail.com>
2018-04-10 11:08:06 -0400

    overlay DMSP SSM data on a map

--- multiinstrument_data_overlay/overlay_DMSP.py
+++ multiinstrument_data_overlay/overlay_DMSP.py
@@ -19,6 +19,11 @@ class DMSP_sat():
                                         sat_num, file_dir=file_dir)
         if data_type == "ssj":
             pass
+        if data_type == "ssm":
+            # Read SSM data from MFR data files obtained from NGDC NOAA
+            from dmsp_ssm_read import dmsp_ssm_read_mfr
+            df = dmsp_ssm_read_mfr(dtm, sat_num, file_dir=file_dir)
+
         self.data = df
 
     def overlay_ssies_data(self, mobj, ax,
@@ -35,7 +40,7 @@ class DMSP_sat():
         import datetime as dt
         import math
         import numpy as np
-        from matplotlib.collections import PolyCollection,LineCollection
+        from matplotlib.collections import LineCollection
     
         # create a colormap for the quality flags of Vx and Vy 
         cmj = matplotlib.cm.jet
@@ -112,6 +117,118 @@ class DMSP_sat():
         ax.add_collection(lcoll)
     
         return
+
+    def overlay_ssm_data(self, mobj, ax, rot_90_clockwise=True,
+                         interval=60, velscl=500.e3):
+
+        """Overlays SSM data onto a map"""
+    
+        import matplotlib
+        import datetime as dt
+        import math
+        import numpy as np
+        from matplotlib.collections import LineCollection
+    
+    
+        verts = [[],[]]
+        tails_dmsp = []
+        # drop NaN values for 'Vy', 'GLAT', and 'GLONG'
+        df = self.data[['Date-time', 'Lat', 'Lon', 'Meas-ModY', 'Meas-ModZ']].dropna()
+        df.set_index('Date-time', inplace=True)
+        df.loc[:, "H"] = np.sqrt(np.square(df['Meas-ModY'].as_matrix()) +\
+                            np.square(df['Meas-ModZ'].as_matrix()))
+        df.loc[:,"theta_H_to_Zdir"] = np.arctan2(df['Meas-ModZ'].as_matrix(),
+                                                 df['Meas-ModY'].as_matrix())
+
+        # Select data for the time of interval
+        df_H = df.loc[self.datetime.strftime("%Y%m%d%H%M%S"):\
+                      (self.datetime+dt.timedelta(seconds=interval-1)).\
+                      strftime("%Y%m%d%H%M%S"), "H"]
+        df_Htheta = df.loc[self.datetime.strftime("%Y%m%d%H%M%S"):\
+                           (self.datetime+dt.timedelta(seconds=interval-1)).\
+                           strftime("%Y%m%d%H%M%S"), "theta_H_to_Zdir"]
+        df_lat = df.loc[self.datetime.strftime("%Y%m%d%H%M%S"):\
+                        (self.datetime+dt.timedelta(seconds=interval-1)).\
+                        strftime("%Y%m%d%H%M%S"), 'Lat']
+        df_lon = df.loc[self.datetime.strftime("%Y%m%d%H%M%S"):\
+                        (self.datetime+dt.timedelta(seconds=interval-1)).\
+                        strftime("%Y%m%d%H%M%S"), 'Lon']
+        xxs, yys = mobj(df_lon[0], df_lat[0], coords='geo')
+        xxe, yye = mobj(df_lon[-1], df_lat[-1], coords='geo')
+        theta_y = math.atan2(yye-yys, xxe-xxs)
+        theta_H = theta_y - df_Htheta.as_matrix()
+        if rot_90_clockwise:
+            theta_H = theta_H - np.deg2rad(90)
+
+        # plot the DMSP path
+        df_lats = df.loc[(self.datetime-dt.\
+                         timedelta(seconds=20*interval)).strftime("%Y%m%d%H%M%S"):\
+                         (self.datetime+dt.timedelta(seconds=20*interval-1)).\
+                         strftime("%Y%m%d%H%M%S"), 'Lat']
+        df_lons = df.loc[(self.datetime-dt.\
+                         timedelta(seconds=20*interval)).strftime("%Y%m%d%H%M%S"):\
+                         (self.datetime+dt.timedelta(seconds=20*interval-1)).\
+                         strftime("%Y%m%d%H%M%S"), 'Lon']
+        for k in range(len(df_lats)):
+            x1s, y1s = mobj(df_lons[k], df_lats[k], coords='geo')
+            mobj.scatter(np.array(x1s), np.array(y1s),
+                         s=1.0, zorder=5, marker='o', color='gray',
+                         edgecolors='face', linewidths=.5)
+
+        # plot the measurement points and velocity vectors at a specified time
+        for k in range(len(df_lat)):
+            x1, y1 = mobj(df_lon[k], df_lat[k], coords='geo')
+            verts[0].append(x1)
+            verts[1].append(y1)
+            x2 = x1+df_H[k]*velscl*(+1.0)*math.cos(theta_H[k])
+            y2 = y1+df_H[k]*velscl*(+1.0)*math.sin(theta_H[k])
+            tails_dmsp.append(((x1,y1),(x2,y2)))
+    
+        xx = mobj.scatter(np.array(verts[0]),np.array(verts[1]),
+                          s=2.5,zorder=5,marker='o',
+                          color="r", edgecolors='face', linewidths=.5)
+        lcoll = LineCollection(np.array(tails_dmsp),
+                               linewidths=0.6, color="g",
+                               zorder=12, alpha=1)
+        ax.add_collection(lcoll)
+    
+        return
+
+if __name__ == "__main__":
+
+    import datetime as dt
+    import matplotlib.pyplot as plt
+    from davitpy.utils import plotUtils
+    
+    dtm = dt.datetime(2002,3,18,16,50)
+    DMSP_sat_num = 13
+    data_type = "ssm"
+    #data_type = "ssies"
+    vmin=200.; vmax=1000.
+    coords = "mlt"
+    
+    fig, ax = plt.subplots(figsize=(12,8))
+    #ax.set_facecolor('black')
+
+    # Plot a map
+    mobj = plotUtils.mapObj(ax=ax, datetime=dtm, lon_0=0., boundinglat=60.,
+                            gridLabels=False, gridLatRes=10., coords=coords,
+                            fillContinents='None')
+    DMSP_obj = DMSP_sat(dtm, DMSP_sat_num, data_type=data_type)
+
+    if data_type == "ssies":
+        DMSP_obj.overlay_ssies_data(mobj, ax,
+                                    interval=2*60, velscl=3*111.,
+                                    vec_cmap="jet",
+                                    vel_scale=[0, 1000.],
+                                    quality_flag=False)
+
+    if data_type == "ssm":
+        DMSP_obj.overlay_ssm_data(mobj, ax, interval=60, velscl=1.e3,
+                                  rot_90_clockwise=True)
+
+    plt.show()
+
 
 
 
